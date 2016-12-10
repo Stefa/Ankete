64b973f9eb08ce8694a808e696e3333b945a6da6
Stefa <milosh.stefanovic@gmail.com>
2016-12-10 17:51:22 +0100

    Refactored login integration test - created login form page class

--- app/integration-tests/login-form.int.spec.ts
+++ app/integration-tests/login-form.int.spec.ts
@@ -9,10 +9,10 @@ import {ApiService} from "../services/api/api.service";
 import {RouterTestingModule} from "@angular/router/testing";
 import {HomeComponent} from "../containers/home/home.component";
 import {ParticipantGuard} from "../guards/participant/participant.guard";
-import {leonardoUserResponse, leonardoUserObject} from '../test/users';
+import { leonardoUserObject } from '../test/users';
 import {By} from "@angular/platform-browser";
 import {MockBackend, MockConnection} from "@angular/http/testing";
-import {Component} from "@angular/core";
+import {Component, DebugElement} from "@angular/core";
 import {Router} from "@angular/router";
 
 @Component({
@@ -21,22 +21,60 @@ import {Router} from "@angular/router";
 })
 class RootCmp {}
 
+class LoginFormPage {
+    loginFormElement;
+
+    usernameInput;
+    passwordInput;
+    form;
+
+    formErrorElement;
+    usernameErrorElement;
+    passwordErrorElement;
+
+    constructor(loginFormDebugElement: DebugElement) {
+        this.loginFormElement = loginFormDebugElement.nativeElement;
+        this.usernameInput = loginFormDebugElement.query(By.css('#username')).nativeElement;
+        this.passwordInput = loginFormDebugElement.query(By.css('#password')).nativeElement;
+        this.form = loginFormDebugElement.query(By.css('form')).nativeElement;
+    }
+
+    private setInput(inputElement, value) {
+        inputElement.value = value;
+        inputElement.dispatchEvent(new Event('input'));
+    }
+
+    setUsername(username) {
+        this.setInput(this.usernameInput, username);
+    }
+    setPassword(password) {
+        this.setInput(this.passwordInput, password);
+    }
+    submitForm() {
+        this.form.dispatchEvent(new Event('submit'));
+    }
+    getErrors() {
+        this.formErrorElement = this.loginFormElement.querySelector('.ui.error.message.login-form');
+        this.usernameErrorElement = this.loginFormElement.querySelector('.ui.error.message.username');
+        this.passwordErrorElement = this.loginFormElement.querySelector('.ui.error.message.password');
+    }
+}
+
 describe('LoginFromIntegration', () => {
     const apiUrl = 'http://localhost:3210';
     let rootFixture;
 
-    function setInput(inputElemtn, value) {
-        inputElemtn.value = value;
-        inputElemtn.dispatchEvent(new Event('input'));
-    }
-
-    function submitForm(form) {
-        form.dispatchEvent(new Event('submit'));
-    }
-
     function advance() {
         tick();
         rootFixture.detectChanges();
+    }
+
+    function goToLogin(router: Router) {
+        rootFixture = TestBed.createComponent(RootCmp);
+        advance();
+
+        router.initialNavigation();
+        advance();
     }
 
     beforeEach(async(() => {
@@ -69,19 +107,12 @@ describe('LoginFromIntegration', () => {
             .compileComponents();
     }));
 
-    beforeEach(() => {
-
-    });
 
     afterEach(() => localStorage.clear());
 
     it('will go to home page when logged in with correct user',
         inject([MockBackend, Location, Router], fakeAsync((backand: MockBackend, location: Location, router: Router) => {
-            rootFixture = TestBed.createComponent(RootCmp);
-            advance();
-
-            router.initialNavigation();
-            advance();
+            goToLogin(router);
             expect(location.path()).toBe('/login');
 
             let getUrl = apiUrl+'/users?username=Leo&password=turtlePower';
@@ -91,16 +122,12 @@ describe('LoginFromIntegration', () => {
                 connection.mockRespond(new Response(response));
             });
 
-            let loginFormDebugElement = rootFixture.debugElement.children[1];
-            let usernameInput = loginFormDebugElement.query(By.css('#username')).nativeElement;
-            let passwordInput = loginFormDebugElement.query(By.css('#password')).nativeElement;
-            let form = loginFormDebugElement.query(By.css('form')).nativeElement;
-            rootFixture.detectChanges();
-            setInput(usernameInput, 'Leo');
-            setInput(passwordInput, 'turtlePower');
+            let loginFormPage = new LoginFormPage(rootFixture.debugElement.children[1]);
+            loginFormPage.setUsername('Leo');
+            loginFormPage.setPassword('turtlePower');
 
             advance();
-            submitForm(form);
+            loginFormPage.submitForm();
 
             tick();
             expect(location.path()).toEqual('/');
@@ -109,11 +136,7 @@ describe('LoginFromIntegration', () => {
 
     it('will stay at login page and show the error message if login was tried with wrong credentials',
         inject([MockBackend, Location, Router], fakeAsync((backand: MockBackend, location: Location, router: Router) => {
-            rootFixture = TestBed.createComponent(RootCmp);
-            advance();
-
-            router.initialNavigation();
-            advance();
+            goToLogin(router);
 
             backand.connections.subscribe((connection: MockConnection) => {
                 let response: ResponseOptions = new ResponseOptions({body: '[]'});
@@ -121,20 +144,18 @@ describe('LoginFromIntegration', () => {
             });
 
             let loginFormDebugElement = rootFixture.debugElement.children[1];
-            let usernameInput = loginFormDebugElement.query(By.css('#username')).nativeElement;
-            let passwordInput = loginFormDebugElement.query(By.css('#password')).nativeElement;
-            let form = loginFormDebugElement.query(By.css('form')).nativeElement;
-            rootFixture.detectChanges();
-            setInput(usernameInput, 'Leo');
-            setInput(passwordInput, '7uRt13P0vEr');
+
+            let loginFormPage = new LoginFormPage(loginFormDebugElement);
+            loginFormPage.setUsername('Leo');
+            loginFormPage.setPassword('7uRt13P0vEr');
 
             advance();
-            submitForm(form);
+            loginFormPage.submitForm();
 
             advance();
             expect(location.path()).toEqual('/login');
-            expect(loginFormDebugElement.nativeElement.querySelector('.ui.error.message.login-form').innerHTML)
-                .toContain('Pogrešno korisničko ime ili šifra!');
+            loginFormPage.getErrors();
+            expect(loginFormPage.formErrorElement.innerHTML).toContain('Pogrešno korisničko ime ili šifra!');
         }))
     );
 });
